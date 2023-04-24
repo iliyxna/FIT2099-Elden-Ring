30b62fd7c02397f335c94ceb780ea6d75abfe0d3
Iliyana <isam0004@student.monash.edu>
2023-04-24 12:01:07 +0800

    Created new sub-package for enemies in the actor package.

---
+++ src/game/actions/BuyItemAction.java
@@ -0,0 +1,43 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.Purchasable;
+import game.actors.Player;
+
+/**
+ * Not used yet
+ */
+public class BuyItemAction extends Action {
+    private Purchasable item;
+
+    public BuyItemAction(Item item){
+        this.item = (Purchasable) item;
+    }
+
+    @Override
+    public String execute(Actor seller, GameMap map) {
+        String retString;
+        Player player = (Player) seller;
+        int playerRune = player.getTotalRunes().getRuneValue();
+        int itemPrice = item.getBuyPrice().getRuneValue();
+
+        // Transaction
+        if (playerRune >= itemPrice){
+            player.addItemToInventory((Item) item);
+            player.subtractRunes(itemPrice);
+            retString = item.toString() + " has been successfully added to inventory.";
+        } else {
+            retString = "Purchase failed. Player does not have enough runes.";
+        }
+        return retString;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return "Buy Item: " + item.toString() + " for $" + item.getBuyPrice() + " from Merchant Kale";
+    }
+}
--- src/game/actors/enemies/LoneWolf.java
+++ src/game/actors/enemies/LoneWolf.java
@@ -1,16 +1,13 @@
-package game.actors;
+package game.actors.enemies;
 
 import edu.monash.fit2099.engine.actions.Action;
 import edu.monash.fit2099.engine.actions.ActionList;
 import edu.monash.fit2099.engine.actions.DoNothingAction;
-import edu.monash.fit2099.engine.actors.Actor;
 import edu.monash.fit2099.engine.displays.Display;
 import edu.monash.fit2099.engine.positions.GameMap;
 import edu.monash.fit2099.engine.weapons.IntrinsicWeapon;
-import game.actions.AttackAction;
+import game.actors.enemies.Enemy;
 import game.behaviours.Behaviour;
-import game.utils.Status;
-import game.behaviours.WanderBehaviour;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,12 +20,12 @@ import java.util.Map;
  * Modified by:
  *
  */
-public class LoneWolf extends Actor {
+public class LoneWolf extends Enemy {
     private Map<Integer, Behaviour> behaviours = new HashMap<>();
 
     public LoneWolf() {
-        super("Lone Wolf", 'h', 102);
-        this.behaviours.put(999, new WanderBehaviour());
+        super("Lone Wolf", 'h', 102, 55, 470);
+//        this.behaviours.put(999, new WanderBehaviour());
     }
 
     /**
@@ -50,25 +47,24 @@ public class LoneWolf extends Actor {
         return new DoNothingAction();
     }
 
-    /**
-     * The lone wolf can be attacked by any actor that has the HOSTILE_TO_ENEMY capability
-     *
-     * @param otherActor the Actor that might be performing attack
-     * @param direction  String representing the direction of the other Actor
-     * @param map        current GameMap
-     * @return
-     */
-    @Override
-    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
-        ActionList actions = new ActionList();
-        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)){
-            actions.add(new AttackAction(this, direction));
-            // HINT 1: The AttackAction above allows you to attak the enemy with your intrinsic weapon.
-            // HINT 1: How would you attack the enemy with a weapon?
-        }
-        return actions;
-    }
-
+//    /**
+//     * The lone wolf can be attacked by any actor that has the HOSTILE_TO_ENEMY capability
+//     *
+//     * @param otherActor the Actor that might be performing attack
+//     * @param direction  String representing the direction of the other Actor
+//     * @param map        current GameMap
+//     * @return
+//     */
+//    @Override
+//    public ActionList allowableActions(Actor otherActor, String direction, GameMap map) {
+//        ActionList actions = new ActionList();
+//        if(otherActor.hasCapability(Status.HOSTILE_TO_ENEMY)){
+//            actions.add(new AttackAction(this, direction));
+//            // HINT 1: The AttackAction above allows you to attack the enemy with your intrinsic weapon.
+//            // HINT 1: How would you attack the enemy with a weapon?
+//        }
+//        return actions;
+//    }
 
     @Override
     public IntrinsicWeapon getIntrinsicWeapon() {
