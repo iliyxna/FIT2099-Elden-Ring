52f368bdf7effeeaba1cfa40de44dda54c043226
nurindamia123 <104429080+nurindamia123@users.noreply.github.com>
2023-05-01 13:32:58 +0800

    updated the player's intrinsic weapon

--- src/game/actors/Player.java
+++ src/game/actors/Player.java
@@ -93,7 +93,7 @@ public class Player extends Actor implements Resettable {
 
 	@Override
 	public IntrinsicWeapon getIntrinsicWeapon() {
-		return new IntrinsicWeapon(1100, "punches", 100);
+		return new IntrinsicWeapon(11, "punches", 100);
 	}
 
 //	@Override
