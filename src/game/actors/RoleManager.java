5e9e4a9015170abb4e409ce872c5c586331219f8
Iliyana <isam0004@student.monash.edu>
2023-05-01 22:41:33 +0800

    Updated rolemanager to throw and catch exceptions.

--- src/game/actors/RoleManager.java
+++ src/game/actors/RoleManager.java
@@ -19,7 +19,22 @@ public class RoleManager {
         System.out.println("1. Samurai Class (Starting weapon: Uchigatana, HP: 455)");
         System.out.println("2. Bandit Class (Starting weapon: Great Knife, HP: 414)");
         System.out.println("3. Wretch Class (Starting weapon: Club, HP: 414)");
-        int choice = Integer.parseInt(sel.nextLine());
+        int choice = 0;
+
+        boolean isValidChoice = false;
+        while (!isValidChoice) {
+            try {
+                choice = Integer.parseInt(sel.nextLine());
+                if (choice < 1 || choice > 3) {
+                    throw new IllegalArgumentException("Invalid choice. Please enter a number between 1 and 3.");
+                }
+                isValidChoice = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a number between 1 and 3.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
         return choice;
     }
 }
