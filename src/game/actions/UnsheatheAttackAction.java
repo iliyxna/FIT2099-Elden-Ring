7dce0650ee1c7f4b77f8742dcaa3b5cff6b7e394
Iliyana <isam0004@student.monash.edu>
2023-05-01 22:20:46 +0800

    Added Quickstep and Unsheathe attack action classes for req 4

---
+++ src/game/actions/UnsheatheAttackAction.java
@@ -0,0 +1,48 @@
+package game.actions;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import edu.monash.fit2099.engine.weapons.WeaponItem;
+import game.actors.enemies.PileOfBones;
+import game.actors.enemies.HeavySkeletalSwordsman;
+
+import java.util.Random;
+
+public class UnsheatheAttackAction extends AttackAction{
+    private Random rand = new Random();
+
+    public UnsheatheAttackAction(Actor target, String direction, WeaponItem weapon){
+        super(target,direction,weapon);
+    }
+
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        if (!(rand.nextInt(100) <= 60)) {
+            return actor + " misses " + this.getTarget() + ".";
+        }
+
+        // Unsheathe deals 2x damage
+        int damage = 2 * this.getWeapon().damage();
+        String result = actor + " " + this.getWeapon().verb() + " " + this.getTarget() + " for " + damage + " damage.";
+        this.getTarget().hurt(damage);
+        // Deals with unconscious actors
+        if (!this.getTarget().isConscious()) {
+            // Deals with spawning and de-spawning of heavy skeletal swordsman
+            if (this.getTarget() instanceof HeavySkeletalSwordsman) {
+                System.out.println("Heavy Skeletal Swordsman turns into Pile of Bones.");
+                Location pos = map.locationOf(this.getTarget());
+                map.removeActor(this.getTarget());
+                map.addActor(new PileOfBones(), pos);
+            } else {
+                result += new DeathAction(actor).execute(this.getTarget(), map);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " performs Unsheathe Skill to " + this.getTarget() + " at " + this.getDirection() + " with " + this.getWeapon() ;
+    }
+}
