7a29524f864d03d7d0ac41f6117f801a55b7acb1
Muhammad Khan <mkha0134@student.monash.edu>
2023-05-03 09:08:32 +0800

    Resettable INTERFACE class has been updated

--- src/game/reset/Resettable.java
+++ src/game/reset/Resettable.java
@@ -1,5 +1,7 @@
 package game.reset;
 
+import edu.monash.fit2099.engine.positions.GameMap;
+
 /**
  * A resettable interface
  * Created by:
@@ -8,5 +10,10 @@ package game.reset;
  *
  */
 public interface Resettable {
-    void reset();
+
+    /**
+     * Method that will be invoked during the reset of the game.
+     * @param map the game map the actor is on.
+     */
+    void reset(GameMap map);
 }
